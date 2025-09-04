dace1dc5857c3ec78bfea38840e35375da25a9ad
Miriã Tomé <miriamtome22@hotmail.com>
2025-09-03 22:32:15 -0300

    Criação do webchat

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const ejs = require("ejs");
+const http = require("http");
+const path = require("path");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "public"));
+
+app.engine("html", ejs.renderFile);
+
+app.use("/", (req, res) => {
+    res.render('index.html');
+});
+
+server.listen(3000, () => {
+  console.log("SERVIDOR RODANDO EM: http://localhost:3000");
+});
